2e512b8c0dd592ebe9570d0425c81043ab687b83
army_jay <chalkyjason@gmail.com>
2022-06-20 23:06:27 -0400

    Added some jave for some fun

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,12 @@
 document.querySelector("#dynamic-content").innerText =
  "This paragraph is dynamically rendered using JavaScript";
+
+var myName = "Jason";
+var yourName = promt("What is your name");
+
+function RunAlert(){
+var myName ="Jason";
+var yourName = prompt("What is your name");
+alert("My name is " + myName + ", welcome to my site " + yourName + "!");
+alert("We are using javascript and variables");
+};
